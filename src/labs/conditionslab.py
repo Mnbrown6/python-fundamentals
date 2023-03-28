2e785ad664c0e7cf05667b0fb36033c73eba91ea
Mahasin Hamilton <Mnbrown6@hotmail.com>
2023-03-27 19:48:10 -0500

    Conditions Lab

---
+++ src/labs/conditionslab.py
@@ -0,0 +1,47 @@
+""" Control Flow Lab"""
+
+""" Task 1 """
+
+def basic_if_else(num1: int):
+    if num1 == 20:
+        print('Value is 35.')
+    else:
+        print('Value is less than 35.')
+
+
+basic_if_else(8)
+
+""" Task 2"""
+
+def school_description(value):
+    if value == 'E':
+        print('Excellent.')
+    elif value == 'V':
+        print('Very Good.')
+    elif value == 'G':
+        print('Good Job.')
+    elif value == 'A':
+        print('Average.')
+    elif value == 'F':
+        print('Failed.')
+    else:
+        print('Not a valid grade.')
+
+
+school_description('a')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
